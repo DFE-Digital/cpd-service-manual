8aac80a8937ad93c7c8acd500c588af0f26f4860
matt <mattonfoot@gmail.com>
2021-04-29 12:45:42 +0100

    update config for relative asset paths

--- config.rb
+++ config.rb
@@ -3,3 +3,9 @@ require 'govuk_tech_docs'
 GovukTechDocs.configure(self)
 
 set :layout, 'custom'
+
+configure :build do
+  # Relative assets needed to deploy to Github Pages
+  activate :relative_assets
+  set :site_url, "/cpd-service-manual"
+end
